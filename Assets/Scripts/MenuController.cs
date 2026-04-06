299566a77d5b2f808e34dd96dfea80edd45e5dab
Miran Güler <gulermiran@gmail.com>
2026-04-06 20:10:44 +0200

    Main Meny prototyp
    Det här är bara en start för main menyn, dock så behövs allt mer settings, men start och quit funkar som normalt

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField] private GameObject settingsPanel;
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void OpenSettingsPanel()
+    {
+        settingsPanel.SetActive(true);
+    }
+
+    public void CloseSettingsPanel()
+    {
+        settingsPanel.SetActive(false);
+    }
+
+}
